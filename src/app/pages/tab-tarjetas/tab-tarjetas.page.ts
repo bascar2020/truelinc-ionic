813552bd82b1e54908747729adae68f75abc1e02
Charlie Molina <bascar2020@gmail.com>
2019-01-06 12:03:19 +1100

    search by empresa name in tarjetsas

--- src/app/pages/tab-tarjetas/tab-tarjetas.page.ts
+++ src/app/pages/tab-tarjetas/tab-tarjetas.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TarjetaDeck } from '../../models/tarjeta.model';
 import { TarjetaService } from '../../services/tarjeta.service';
 import { LoaderService } from '../../services/loader.service';
+import { ToastService } from 'src/app/services/toast.service';
 
 
 @Component({
@@ -11,10 +12,12 @@ import { LoaderService } from '../../services/loader.service';
 })
 export class TabTarjetasPage implements OnInit {
   misTarjetas: TarjetaDeck[];
+  copyOfMisTarjetas: TarjetaDeck[];
 
   constructor(
     private tarjetaService: TarjetaService,
     private loadingService: LoaderService,
+    private toast: ToastService,
   ) { }
 
   ngOnInit() {
@@ -24,15 +27,30 @@ export class TabTarjetasPage implements OnInit {
   private getTarjetas() {
     this.loadingService.presentLoading();
     this.tarjetaService.getTarjetasCurrentUser().subscribe((tarjetas) => {
-      this.misTarjetas = tarjetas;
+      console.log(tarjetas);
+      this.misTarjetas = tarjetas.map((t) => {
+        return {
+              id: t.id,
+              nombre: t.get('Nombre'),
+              empresa: t.get('Empresa'),
+              cargo: t.get('Cargo'),
+              logo: (t.get('LogoEmpresa') === undefined ? 'assets/img/noImage.jpg' : t.get('LogoEmpresa').url())
+            };
+      });
+      this.copyOfMisTarjetas = Array.from(this.misTarjetas);
       this.loadingService.dissminsLoading();
+    }, () => {
+      this.loadingService.dissminsLoading();
+      this.toast.presentErrorToast('Error al optener tus cartas');
     });
   }
 
   generateUrl(tarjetaCode: string): string {
     return `/home/tabs/tarjetas/${tarjetaCode}`;
   }
-
+  filteredTarjetas(tarjetasFiltered: TarjetaDeck[]) {
+    this.misTarjetas = tarjetasFiltered;
+  }
 
 
 }
--- src/app/services/loader.service.ts
+++ src/app/services/loader.service.ts
@@ -19,9 +19,8 @@ export class LoaderService {
         translucent: true,
       }).then(a => {
         a.present().then(() => {
-          console.log('presented');
           if (!this.isLoading) {
-            a.dismiss().then(() => console.log('abort presenting'));
+            a.dismiss().then();
           }
         });
       });
@@ -30,6 +29,6 @@ export class LoaderService {
 
   async dissminsLoading() {
     this.isLoading = false;
-    return await this.loadingController.dismiss().then(() => console.log('dismissed'));
+    return await this.loadingController.dismiss().then();
   }
 }
